13b9f42ec0c4aca2b9b30282f3fc4119bafbe3c6
ActuallyBearTV <louissanderson2025@gmail.com>
2026-06-29 19:18:26 +0100

    Create ticket.js

---
+++ src/commands/ticket.js
@@ -0,0 +1,61 @@
+const {
+  ChannelType,
+  PermissionFlagsBits
+} = require("discord.js");
+
+const { havenEmbed } = require("../utils/embed");
+
+module.exports = {
+  name: "ticket",
+
+  async execute(interaction) {
+    const guild = interaction.guild;
+    const member = interaction.member;
+
+    const existingTicket = guild.channels.cache.find(
+      channel => channel.name === `ticket-${member.user.username.toLowerCase()}`
+    );
+
+    if (existingTicket) {
+      return interaction.reply({
+        content: `You already have an open ticket: ${existingTicket}`,
+        ephemeral: true
+      });
+    }
+
+    const ticketChannel = await guild.channels.create({
+      name: `ticket-${member.user.username}`,
+      type: ChannelType.GuildText,
+      permissionOverwrites: [
+        {
+          id: guild.roles.everyone.id,
+          deny: [PermissionFlagsBits.ViewChannel]
+        },
+        {
+          id: member.id,
+          allow: [
+            PermissionFlagsBits.ViewChannel,
+            PermissionFlagsBits.SendMessages,
+            PermissionFlagsBits.ReadMessageHistory
+          ]
+        }
+      ]
+    });
+
+    await ticketChannel.send({
+      content: `${member}`,
+      embeds: [
+        havenEmbed(
+          "🎫 Ticket Opened",
+          "Please explain what you need help with. A staff member will reply as soon as possible.",
+          "#60A5FA"
+        )
+      ]
+    });
+
+    return interaction.reply({
+      content: `Ticket created: ${ticketChannel}`,
+      ephemeral: true
+    });
+  }
+};
