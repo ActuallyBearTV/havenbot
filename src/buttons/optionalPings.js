8910d3ae706d508e9cf6dd1602a5e7d2df6cfc0f
ActuallyBearTV <louissanderson2025@gmail.com>
2026-06-29 18:43:51 +0100

    Update optionalPings.js

--- src/buttons/optionalPings.js
+++ src/buttons/optionalPings.js
@@ -1,1 +1,39 @@
+const { OPTIONAL_PINGS } = require("../config/constants");
+const { findRole } = require("../utils/finders");
 
+async function toggleOptionalPing(interaction) {
+  const selectedPing = OPTIONAL_PINGS.find(
+    ping => ping.id === interaction.customId
+  );
+
+  if (!selectedPing) return;
+
+  const selectedRole = findRole(interaction.guild, selectedPing.name);
+
+  if (!selectedRole) {
+    return interaction.reply({
+      content: "That notification role does not exist yet. Ask staff to run `/setup-optional-pings`.",
+      ephemeral: true
+    });
+  }
+
+  if (interaction.member.roles.cache.has(selectedRole.id)) {
+    await interaction.member.roles.remove(selectedRole);
+
+    return interaction.reply({
+      content: `Removed ${selectedPing.name}.`,
+      ephemeral: true
+    });
+  }
+
+  await interaction.member.roles.add(selectedRole);
+
+  return interaction.reply({
+    content: `Added ${selectedPing.name}.`,
+    ephemeral: true
+  });
+}
+
+module.exports = {
+  toggleOptionalPing
+};
