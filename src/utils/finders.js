08b61ba7152b8e88907f7c453745fcb44feb5d21
ActuallyBearTV <louissanderson2025@gmail.com>
2026-06-29 18:30:08 +0100

    Update finders.js

--- src/utils/finders.js
+++ src/utils/finders.js
@@ -1,1 +1,18 @@
+function findChannel(guild, name) {
+  return guild.channels.cache.find(
+    channel =>
+      channel.name === name ||
+      channel.name === name.toLowerCase()
+  );
+}
 
+function findRole(guild, name) {
+  return guild.roles.cache.find(
+    role => role.name === name
+  );
+}
+
+module.exports = {
+  findChannel,
+  findRole
+};
