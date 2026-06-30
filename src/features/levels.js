bf5126e9b5d07390c76d266842b98f4ea891f46e
ActuallyBearTV <louissanderson2025@gmail.com>
2026-06-30 09:29:33 +0100

    Update levels.js

--- src/features/levels.js
+++ src/features/levels.js
@@ -1,16 +1,4 @@
-const Database = require("better-sqlite3");
-const fs = require("fs");
-const path = require("path");
-
-const dataDir = process.env.RAILWAY_VOLUME_MOUNT_PATH || path.join(__dirname, "../../data");
-
-if (!fs.existsSync(dataDir)) {
-  fs.mkdirSync(dataDir, { recursive: true });
-}
-
-console.log("Database path:", path.join(dataDir, "haven.db"));
-
-const db = new Database(path.join(dataDir, "haven.db"));
+const db = require("../database/database");
 db.prepare(`
   CREATE TABLE IF NOT EXISTS levels (
     guild_id TEXT NOT NULL,
