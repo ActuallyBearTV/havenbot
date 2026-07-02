a226bc62b3b0035fb2208cb8e7237c643a10209d
ActuallyBearTV <louissanderson2025@gmail.com>
2026-07-02 19:50:11 +0100

    Update slowmode.js

--- src/commands/slowmode.js
+++ src/commands/slowmode.js
@@ -1,5 +1,12 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const { SlashCommandBuilder } = require("discord.js");
 const { sendStaffLog } = require("../utils/staffLogs");
+
+const allowedRoles = [
+  "1521148642419933305", // Trial Mod
+  "1521148641325223936", // Moderator
+  "1521148639559553044", // Senior Moderator
+  "1521148638556983428"  // Admin
+];
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -12,18 +19,29 @@ module.exports = {
         .setRequired(true)
         .setMinValue(0)
         .setMaxValue(21600)
-    )
-    .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
+    ),
 
   async execute(interaction) {
+    const hasAllowedRole = interaction.member.roles.cache.some(role =>
+      allowedRoles.includes(role.id)
+    );
+
+    if (!hasAllowedRole) {
+      return interaction.reply({
+        content: "❌ You don't have permission to use this command.",
+        ephemeral: true
+      });
+    }
+
     const seconds = interaction.options.getInteger("seconds");
 
     await interaction.channel.setRateLimitPerUser(seconds);
 
     await interaction.reply({
-      content: seconds === 0
-        ? `✅ Slowmode disabled in ${interaction.channel}.`
-        : `⏱️ Slowmode set to **${seconds} seconds** in ${interaction.channel}.`
+      content:
+        seconds === 0
+          ? `✅ Slowmode disabled in ${interaction.channel}.`
+          : `⏱️ Slowmode set to **${seconds} seconds** in ${interaction.channel}.`
     });
 
     await sendStaffLog(interaction.guild, {
