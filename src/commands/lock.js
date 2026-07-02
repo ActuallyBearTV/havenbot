9d87358d5969b99f76866190daf783820d9d6a1a
ActuallyBearTV <louissanderson2025@gmail.com>
2026-07-02 21:19:45 +0100

    Update lock.js

--- src/commands/lock.js
+++ src/commands/lock.js
@@ -1,20 +1,6 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const { SlashCommandBuilder } = require("discord.js");
 const { sendStaffLog } = require("../utils/staffLogs");
-
-const allowedRoles = [
-  "1521148642419933305", // Trial Mod
-  "1521148641325223936", // Moderator
-  "1521148639559553044", // Senior Moderator
-  "1521148638556983428",  // Admin
-  "1521148635448868974" // Owner
-];
-
-function hasStaffPermission(interaction) {
-  return (
-    interaction.member.permissions.has(PermissionFlagsBits.Administrator) ||
-    interaction.member.roles.cache.some(role => allowedRoles.includes(role.id))
-  );
-}
+const { hasStaffPermission, staffRoleIds } = require("../utils/staffPermissions");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -44,7 +30,7 @@ module.exports = {
       if (
         role.id === interaction.guild.id ||
         role.managed ||
-        allowedRoles.includes(role.id)
+        staffRoleIds.includes(role.id)
       ) continue;
 
       await channel.permissionOverwrites.edit(role, {
