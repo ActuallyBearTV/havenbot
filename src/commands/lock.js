bc11d6a6cbe574530ec739fac5286274efb42584
ActuallyBearTV <louissanderson2025@gmail.com>
2026-07-02 21:25:18 +0100

    Update lock.js

--- src/commands/lock.js
+++ src/commands/lock.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require("discord.js");
 const { sendStaffLog } = require("../utils/staffLogs");
-const { hasStaffPermission, staffRoleIds } = require("../utils/staffPermissions");
+const { hasStaffPermission } = require("../utils/staffPermissions");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -15,8 +15,6 @@ module.exports = {
       });
     }
 
-    await interaction.deferReply();
-
     const channel = interaction.channel;
 
     await channel.permissionOverwrites.edit(interaction.guild.roles.everyone, {
@@ -26,22 +24,7 @@ module.exports = {
       CreatePrivateThreads: false
     });
 
-    for (const role of interaction.guild.roles.cache.values()) {
-      if (
-        role.id === interaction.guild.id ||
-        role.managed ||
-        staffRoleIds.includes(role.id)
-      ) continue;
-
-      await channel.permissionOverwrites.edit(role, {
-        SendMessages: false,
-        SendMessagesInThreads: false,
-        CreatePublicThreads: false,
-        CreatePrivateThreads: false
-      }).catch(() => null);
-    }
-
-    await interaction.editReply({
+    await interaction.reply({
       content: `🔒 ${channel} has been locked.`
     });
 
