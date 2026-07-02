605d844582fd94674195a9e23901a895285df2dd
ActuallyBearTV <louissanderson2025@gmail.com>
2026-07-02 21:27:22 +0100

    Update unlock.js

--- src/commands/unlock.js
+++ src/commands/unlock.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require("discord.js");
 const { sendStaffLog } = require("../utils/staffLogs");
-const { hasStaffPermission, staffRoleIds } = require("../utils/staffPermissions");
+const { hasStaffPermission } = require("../utils/staffPermissions");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -15,36 +15,16 @@ module.exports = {
       });
     }
 
-    await interaction.deferReply();
-
     const channel = interaction.channel;
 
-    await channel.permissionOverwrites.edit(
-      interaction.guild.roles.everyone,
-      {
-        SendMessages: null,
-        SendMessagesInThreads: null,
-        CreatePublicThreads: null,
-        CreatePrivateThreads: null
-      }
-    );
+    await channel.permissionOverwrites.edit(interaction.guild.roles.everyone, {
+      SendMessages: null,
+      SendMessagesInThreads: null,
+      CreatePublicThreads: null,
+      CreatePrivateThreads: null
+    });
 
-    for (const role of interaction.guild.roles.cache.values()) {
-      if (
-        role.id === interaction.guild.id ||
-        role.managed ||
-        staffRoleIds.includes(role.id)
-      ) continue;
-
-      await channel.permissionOverwrites.edit(role, {
-        SendMessages: null,
-        SendMessagesInThreads: null,
-        CreatePublicThreads: null,
-        CreatePrivateThreads: null
-      }).catch(() => null);
-    }
-
-    await interaction.editReply({
+    await interaction.reply({
       content: `🔓 ${channel} has been unlocked.`
     });
 
