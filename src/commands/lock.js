c92a18e34c68121b2d14794a4a36fa5a62c44623
ActuallyBearTV <louissanderson2025@gmail.com>
2026-07-02 19:51:54 +0100

    Update lock.js

--- src/commands/lock.js
+++ src/commands/lock.js
@@ -1,15 +1,35 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const { SlashCommandBuilder, PermissionsBitField } = require("discord.js");
 const { sendStaffLog } = require("../utils/staffLogs");
+
+const allowedRoles = [
+  "1521148642419933305", // Trial Mod
+  "1521148641325223936", // Moderator
+  "1521148639559553044", // Senior Moderator
+  "1521148638556983428"  // Admin
+];
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("lock")
-    .setDescription("Lock this channel so members cannot send messages.")
-    .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
+    .setDescription("Lock this channel so members cannot send messages."),
 
   async execute(interaction) {
+    const hasAllowedRole = interaction.member.roles.cache.some(role =>
+      allowedRoles.includes(role.id)
+    );
+
+    if (!hasAllowedRole) {
+      return interaction.reply({
+        content: "❌ You don't have permission to use this command.",
+        ephemeral: true
+      });
+    }
+
     await interaction.channel.permissionOverwrites.edit(interaction.guild.roles.everyone, {
-      SendMessages: false
+      SendMessages: false,
+      SendMessagesInThreads: false,
+      CreatePublicThreads: false,
+      CreatePrivateThreads: false
     });
 
     await interaction.reply({
