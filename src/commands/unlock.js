4070edf9b5ad9c8aa2f27263aa8b83c13be1b242
ActuallyBearTV <louissanderson2025@gmail.com>
2026-07-02 19:53:20 +0100

    Update unlock.js

--- src/commands/unlock.js
+++ src/commands/unlock.js
@@ -1,17 +1,37 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const { SlashCommandBuilder } = require("discord.js");
 const { sendStaffLog } = require("../utils/staffLogs");
+
+const allowedRoles = [
+  "1521148642419933305", // Trial Mod
+  "1521148641325223936", // Moderator
+  "1521148639559553044", // Senior Moderator
+  "1521148638556983428"  // Admin
+];
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("unlock")
-    .setDescription("Unlock this channel.")
-    .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
+    .setDescription("Unlock this channel."),
 
   async execute(interaction) {
+    const hasAllowedRole = interaction.member.roles.cache.some(role =>
+      allowedRoles.includes(role.id)
+    );
+
+    if (!hasAllowedRole) {
+      return interaction.reply({
+        content: "❌ You don't have permission to use this command.",
+        ephemeral: true
+      });
+    }
+
     await interaction.channel.permissionOverwrites.edit(
       interaction.guild.roles.everyone,
       {
-        SendMessages: null
+        SendMessages: null,
+        SendMessagesInThreads: null,
+        CreatePublicThreads: null,
+        CreatePrivateThreads: null
       }
     );
 
