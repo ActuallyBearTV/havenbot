aa63a4df7e67138f4c1cdd21165462967c8fbd35
ActuallyBearTV <louissanderson2025@gmail.com>
2026-07-02 19:55:06 +0100

    Update removewarn.js

--- src/commands/removewarn.js
+++ src/commands/removewarn.js
@@ -1,9 +1,16 @@
 const fs = require("node:fs");
 const path = require("node:path");
-const { SlashCommandBuilder, PermissionFlagsBits } = require("discord.js");
+const { SlashCommandBuilder } = require("discord.js");
 const { sendStaffLog } = require("../utils/staffLogs");
 
 const warningsPath = path.join(__dirname, "..", "data", "warnings.json");
+
+const allowedRoles = [
+  "1521148642419933305", // Trial Mod
+  "1521148641325223936", // Moderator
+  "1521148639559553044", // Senior Moderator
+  "1521148638556983428"  // Admin
+];
 
 function loadWarnings() {
   if (!fs.existsSync(warningsPath)) return {};
@@ -19,14 +26,30 @@ module.exports = {
     .setName("removewarn")
     .setDescription("Remove a specific warning from a member.")
     .addUserOption(option =>
-      option.setName("user").setDescription("The member").setRequired(true)
+      option
+        .setName("user")
+        .setDescription("The member")
+        .setRequired(true)
     )
     .addStringOption(option =>
-      option.setName("warning-id").setDescription("The warning ID to remove").setRequired(true)
-    )
-    .setDefaultMemberPermissions(PermissionFlagsBits.ModerateMembers),
+      option
+        .setName("warning-id")
+        .setDescription("The warning ID to remove")
+        .setRequired(true)
+    ),
 
   async execute(interaction) {
+    const hasAllowedRole = interaction.member.roles.cache.some(role =>
+      allowedRoles.includes(role.id)
+    );
+
+    if (!hasAllowedRole) {
+      return interaction.reply({
+        content: "❌ You don't have permission to use this command.",
+        ephemeral: true
+      });
+    }
+
     const user = interaction.options.getUser("user");
     const warningId = interaction.options.getString("warning-id");
 
