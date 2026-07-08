51fe7cd6d3704b3d1cd136d1741eb79dce986489
ActuallyBearTV <louissanderson2025@gmail.com>
2026-07-08 18:38:21 +0100

    Create channels.js

---
+++ src/config/channels.js
@@ -0,0 +1,35 @@
+module.exports = {
+  INFO: {
+    WELCOME: "",
+    RULES: "",
+    ANNOUNCEMENTS: "",
+    UPDATES: "",
+    VERIFY: ""
+  },
+
+  ROLES: {
+    COLOUR_ROLES: "",
+    NOTIFICATION_ROLES: "",
+    SELF_ROLES: ""
+  },
+
+  COMMUNITY: {
+    GENERAL: "",
+    LEVELS: "",
+    SUGGESTIONS: "",
+    QUOTES: "",
+    STARBOARD: ""
+  },
+
+  STAFF: {
+    STAFF_LOGS: "",
+    MOD_LOGS: "",
+    TICKETS: ""
+  },
+
+  STATS: {
+    MEMBERS: "",
+    BOOSTS: "",
+    ONLINE: ""
+  }
+};
