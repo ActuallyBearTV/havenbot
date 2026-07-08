b102b7917cd562fb7ab69873f7608212b0067686
ActuallyBearTV <louissanderson2025@gmail.com>
2026-07-08 12:28:04 +0100

    Create roles.js

---
+++ src/config/roles.js
@@ -0,0 +1,110 @@
+module.exports = {
+  STAFF: {
+    OWNER: "",
+    CO_OWNER: "",
+    ADMIN: "",
+    SENIOR_MODERATOR: "",
+    MODERATOR: "",
+    TRIAL_MOD: ""
+  },
+
+  COLOURS: {
+    CRIMSON: "",
+    PINK: "",
+    PURPLE: "",
+    BLUE: "",
+    CYAN: "",
+    GREEN: "",
+    YELLOW: "",
+    ORANGE: "",
+    BROWN: "",
+    WHITE: "",
+    GREY: "",
+    BLACK: ""
+  },
+  
+  RELATIONSHIP_STATUS: {
+    TAKEN: "",
+    SINGLE: "",
+    COMPLICATED: "",
+    NOT_LOOKING: "",
+    LOOKING: ""
+  },
+
+  PINGS: {
+    CHAT_REVIVE: "",
+    VC_REVIVE: "",
+    DAILY_QUESTION: "",
+    ANNOUNCEMENT: "",
+    BUMP_REMINDER: "",
+    GIVEAWAY_PING: ""
+  },
+
+  GENDER: {
+    MALE: "",
+    FEMALE: "",
+    NON_BINARY: "",
+    GENDER_FLUID: "",
+    TRANSGENDER: "",
+    OTHER: ""
+  },
+
+  AGE: {
+    AGE_18_20: "",
+    AGE_21_24: "",
+    AGE_25_27: "",
+    AGE_28_30: "",
+    AGE_30PLUS: ""
+  },
+  
+  DMS: {
+    DMS_OPEN: "",
+    DMS_CLOSED: "",
+    ASK_TO_DM: "",
+  },
+  
+  LOCATION: {
+    UK: "",
+    NORTH_AMERICA: "",
+    SOUTH_AMERICA: "",
+    EUROPE: "",
+    ASIA: "",
+    AFRICA: "",
+    OCEANIA: ""
+  },
+
+  INTERESTS: {
+    GAMER: "",
+    MUSIC: "",
+    MOVIE: "",
+    ANIME: "",
+    PET: "",
+    TECH: "",
+    CREATIVE: "",
+    NIGHT_OWL: ""
+  },
+  
+  GAMES: {
+    MOBILE: "",
+    PC: "",
+    XBOX: "",
+    PLAYSTATION: "",
+    SWITCH: "",
+    ROBLOX: "",
+    VALORANT: "",
+    MINECRAFT: "",
+    FORTNITE: "",
+    CALL_OF_DUTY: "",
+    GTA: "",
+    OVERWATCH: "",
+    MARVEL_RIVALS: "",
+    DEAD_BY_DAYLIGHT: "",
+    VR_CHAT: "".
+  },
+
+  MISC: {
+    VERIFIED: "",
+    MEMBER: "",
+    OG: ""
+  }
+};
