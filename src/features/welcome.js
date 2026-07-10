0c41018dcf950ad2e50677d3ecf91f1d1aa7ae4c
ActuallyBearTV <louissanderson2025@gmail.com>
2026-07-10 20:29:01 +0100

    Update welcome.js

--- src/features/welcome.js
+++ src/features/welcome.js
@@ -15,13 +15,12 @@ async function sendWelcome(member) {
       havenEmbed(
         "<:174f_moon:1525205327916564550> Welcome to Haven | 18+ ",
         [
-          `Welcome ${member}! <a:002_heart:1525205472779571300>
-`,
+          `Welcome ${member}! <a:002_heart:1525205472779571300>`,
           "",
           "We're so happy you've joined **Haven**.",
           "",
           "˖ ࣪ ⊹ read the ⁠<#1521148685394772099>",
-          "**╭₊˚๑ before you start chatting : ₊˚੭ 
+          "**╭₊˚๑ before you start chatting : ₊˚੭, 
           "make sure you do the following..**",
           "˖ ࣪ ⊹ read the ⁠<#1521148685394772099>",
           "˖ ࣪ ⊹ checkout our ⁠•₊˚๑<#1521148695146397867>ˎˊ˗",
