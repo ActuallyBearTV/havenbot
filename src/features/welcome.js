9c92ba5e1150a33afd6534f8c2103580a881da27
ActuallyBearTV <louissanderson2025@gmail.com>
2026-07-10 20:32:48 +0100

    Update welcome.js

--- src/features/welcome.js
+++ src/features/welcome.js
@@ -2,7 +2,9 @@ const { havenEmbed } = require("../utils/embed");
 const Channels = require("../config/channels");
 
 async function sendWelcome(member) {
-  const channel = member.guild.channels.cache.get(Channels.COMMUNITY.GENERAL);
+  const channel = member.guild.channels.cache.get(
+    Channels.COMMUNITY.GENERAL
+  );
 
   if (!channel) {
     console.log("❌ Welcome channel not found.");
@@ -13,19 +15,19 @@ async function sendWelcome(member) {
     content: `${member}`,
     embeds: [
       havenEmbed(
-        "<:174f_moon:1525205327916564550> Welcome to Haven | 18+ ",
+        "<:174f_moon:1525205327916564550> Welcome to Haven | 18+",
         [
           `Welcome ${member}! <a:002_heart:1525205472779571300>`,
           "",
           "We're so happy you've joined **Haven**.",
           "",
-          "˖ ࣪ ⊹ read the ⁠<#1521148685394772099>",
-          "**╭₊˚๑ before you start chatting : ₊˚੭, 
-          "make sure you do the following..**",
-          "˖ ࣪ ⊹ read the ⁠<#1521148685394772099>",
-          "˖ ࣪ ⊹ checkout our ⁠•₊˚๑<#1521148695146397867>ˎˊ˗",
-          "˖ ࣪ ⊹ introduce yourself and have fun ",
-        
+          "**╭₊˚๑ before you start chatting : ₊˚੭**",
+          "make sure you do the following...",
+          "",
+          "˖ ࣪ ⊹ read the rules in <#1521148685394772099>",
+          "˖ ࣪ ⊹ check out <#1521148695146397867>",
+          "˖ ࣪ ⊹ introduce yourself and have fun",
+          "",
           `You are member **#${member.guild.memberCount}**.`,
           "",
           "We hope you enjoy your stay! <:1ivys_heart:1525205802099540061>"
