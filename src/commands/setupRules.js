2d6976c5b2fac296543b669dd231de71119a7e2a
ActuallyBearTV <louissanderson2025@gmail.com>
2026-07-10 19:37:05 +0100

    Update setupRules.js

--- src/commands/setupRules.js
+++ src/commands/setupRules.js
@@ -66,69 +66,83 @@ module.exports = {
         })
         .setDescription(
           [
-            :purple_moon:  We're an adult server, aiming to create a safe environment for adults to make genuine friendships and be part of a welcoming community. Please make sure to read the rules.
-
-:Yumii_008whiterollstar:1. Be respectful to everyone in chat and vc. (including staff)
-๑ If someone asks you to stop talking about a topic please respect their wishes.
-๑ No body shaming, bullying, sexism, homophobia or any other form of discrimination will be tolerated.
-
-:Yumii_008whiterollstar:2. No slurs/derogatory remarks.
-๑ They have been blocked off by a bot, please don't try to bypass them.
-๑ Bypassing them will result in a warning.
-
-:Yumii_008whiterollstar:3. No names, users, pronoun names, banners, pfps, statuses that might be offensive. Failure to change these upon request will result in a removal from the server.
-
-:Yumii_008whiterollstar:4. Don’t make fun of or talk badly of anyone based on their beliefs, race, religion or appearance.
-
-:Yumii_008whiterollstar:5. No minors in the server.
-๑ This is an 18+ server, if you suspect someone isn’t of age please let a staff member know.
-
-:Yumii_008whiterollstar:6. Harassment, doxing, blackmailing, stalking or unrequested inappropriate dms will result in a ban.
-๑ Don’t leak/share anyone’s personal information or pictures they send you privately.
-๑ Don’t go looking for any members’ information if they haven’t shared it with you.
-๑ Unrequested dms include sexual/sensitive pictures or messages you specifically did not say yes to or agreed to receive. (must show evidence)
-
-:Yumii_008whiterollstar:7. No jokes about racism, racial stereotypes, rape, suicide, self-harm, pedo, disturbing or sensitive topics.
-
-:Yumii_008whiterollstar:8. Don’t bring up politics, controversial/sensitive topics.
-๑ This includes but isn't limited to killing, hard drugs, suicide, abuse, abortion and rape.
-๑ If you’d like to vent please use the mental health channel and add spoilers to any triggering words.
-
-:Yumii_008whiterollstar:9. No images, links or videos containing nudity, sexual activity, scam tactics or violence in any of the chats.
-
-:Yumii_008whiterollstar:10. No drama. Don’t bring your issues to the server and solve them in private.
-๑ If someone unadds you/blocks you, don’t bring it up to the server.
-๑ We also won't be dealing with situations that happen outside of the server, for example in group chats or other servers, for our own sanity. We can only control and deal with what happens inside of this server.
-๑ Arguments/disagreements are normal, but don’t get disrespectful or continue to bring up an issue multiple times. If this happens you will be asked to stop, If it continues you will be warned or put in timeout.
-๑ Don't share/stream private dms in the server.
-
-:Yumii_008whiterollstar:11. No nsfw content.
-๑ Small talk or jokes that don’t offend others are okay.
-
-:Yumii_008whiterollstar: 12. No promotions of other servers or invites. We only allow this if you've partnered up with us.
-๑ Don’t advertise other servers in any of the channels. This will result in you getting kicked off the server. 
-
-˗ˏˋ ꒰ Warnings/Timeouts/Bans/Appeals ꒱ ˎˊ˗
-:Yumii_008whiterollstar:Strike system:
-๑ 2 warnings before kicking out
-๑ Kick/Ban
-
-:Yumii_008whiterollstar:Timeouts: Causing drama or talking about a topic after being asked to stop, will result in you being muted.
-
-:Yumii_008whiterollstar:Kicks: We will kick anyone who is problematic, wants to bring drama, advertises other servers through dms, causes issues to other members and breaks the rules.
-
-:Yumii_008whiterollstar:Automatic Bans: Hate speech and imagery, racist comments, threats, trolls, scamming in server or dms, unwanted predatory behaviour (with evidence) and inappropriate behavior with minors.
-(Staff will not be dealing with anyone being childish, rude, problematic or unreasonable in dms after being banned).
-
-:Yumii_008whiterollstar:Appeals:
-๑ Warnings: Open a ticket in the ticket channel.
-๑ Kicks: Open a ticket in the ticket channel.
-๑ Timeouts: contact a staff member.
-๑ Bans: contact a staff member. (Bans for discord ToS violations cannot be appealed).
-
-╭₊˚๑ boost perks : ₊˚੭
-:Yumii_008whiterollstar:booster icon
-:Yumii_008whiterollstar:customisable role (pick your own role name, colour, icon)
+            .setDescription(
+  [
+    "<:purple_moon:1523984549632016414> **we're an adult server**, aiming to create a safe environment for adults to make genuine friendships and be part of a welcoming community. please make sure to read the rules.",
+    "",
+    "<a:Yumii_008whiterollstar:1525122138779156580> **1. be respectful to everyone in chat and vc, including staff.**",
+    "๑ if someone asks you to stop talking about a topic, please respect their wishes.",
+    "๑ no body shaming, bullying, sexism, homophobia or any other form of discrimination will be tolerated.",
+    "",
+    "<a:Yumii_008whiterollstar:1525122138779156580> **2. no slurs or derogatory remarks.**",
+    "๑ they have been blocked by the bot, so please don't try to bypass them.",
+    "๑ bypassing them will result in a warning.",
+    "",
+    "<a:Yumii_008whiterollstar:1525122138779156580> **3. keep profiles appropriate.**",
+    "names, usernames, pronoun names, banners, profile pictures and statuses must not be offensive. failure to change them when requested may result in removal from the server.",
+    "",
+    "<a:Yumii_008whiterollstar:1525122138779156580> **4. respect people's beliefs and identity.**",
+    "don't make fun of or talk badly about anyone based on their beliefs, race, religion or appearance.",
+    "",
+    "<a:Yumii_008whiterollstar:1525122138779156580> **5. no minors in the server.**",
+    "๑ this is an 18+ server. if you suspect someone is underage, let a staff member know.",
+    "",
+    "<a:Yumii_008whiterollstar:1525122138779156580> **6. harassment and privacy violations are forbidden.**",
+    "harassment, doxxing, blackmailing, stalking or unrequested inappropriate dms will result in a ban.",
+    "๑ don't leak or share anyone's personal information or pictures they sent privately.",
+    "๑ don't go looking for members' information if they haven't shared it.",
+    "๑ unwanted dms include sexual or sensitive images or messages the recipient did not agree to receive. evidence must be provided.",
+    "",
+    "<a:Yumii_008whiterollstar:1525122138779156580> **7. no jokes about disturbing or sensitive subjects.**",
+    "this includes racism, racial stereotypes, rape, suicide, self-harm, paedophilia and similar topics.",
+    "",
+    "<a:Yumii_008whiterollstar:1525122138779156580> **8. avoid politics and controversial topics.**",
+    "๑ this includes killing, hard drugs, suicide, abuse, abortion and rape.",
+    "๑ use the mental health channel for venting and add spoilers to triggering words.",
+    "",
+    "<a:Yumii_008whiterollstar:1525122138779156580> **9. no unsafe content.**",
+    "do not post images, links or videos containing nudity, sexual activity, scams or graphic violence.",
+    "",
+    "<a:Yumii_008whiterollstar:1525122138779156580> **10. no drama.**",
+    "keep personal issues private.",
+    "๑ if someone blocks or removes you, don't bring it into the server.",
+    "๑ staff will only deal with incidents that happen inside haven.",
+    "๑ disagreements are normal, but do not become disrespectful or repeatedly restart arguments.",
+    "๑ don't share or stream private dms in the server.",
+    "",
+    "<a:Yumii_008whiterollstar:1525122138779156580> **11. no nsfw content.**",
+    "๑ small jokes are fine provided they do not offend others, but explicit content is not allowed.",
+    "",
+    "<a:Yumii_008whiterollstar:1525122138779156580> **12. no server promotions or invites.**",
+    "advertising is only allowed for official partners.",
+    "๑ advertising other servers may result in removal from haven.",
+    "",
+    "˗ˏˋ ꒰ **warnings, timeouts, bans and appeals** ꒱ ˎˊ˗",
+    "",
+    "<a:Yumii_008whiterollstar:1525122138779156580> **strike system**",
+    "๑ two warnings may lead to a kick.",
+    "๑ severe behaviour may result in an immediate kick or ban.",
+    "",
+    "<a:Yumii_008whiterollstar:1525122138779156580> **timeouts**",
+    "causing drama or continuing a topic after being asked to stop may result in a timeout.",
+    "",
+    "<a:Yumii_008whiterollstar:1525122138779156580> **kicks**",
+    "members may be kicked for repeated drama, advertising, harassment, causing issues or repeatedly breaking rules.",
+    "",
+    "<a:Yumii_008whiterollstar:1525122138779156580> **automatic bans**",
+    "hate speech or imagery, racist comments, threats, trolling, scamming, predatory behaviour and inappropriate behaviour involving minors may result in an immediate ban.",
+    "",
+    "<a:Yumii_008whiterollstar:1525122138779156580> **appeals**",
+    "๑ warnings: open a ticket.",
+    "๑ kicks: open a ticket.",
+    "๑ timeouts: contact a staff member.",
+    "๑ bans: contact a staff member. bans for discord tos violations cannot be appealed.",
+    "",
+    "╭₊˚๑ **boost perks** ₊˚੭",
+    "๑ booster icon",
+    "๑ customisable role with your own name, colour and icon"
+  ].join("\n")
+)
           ].join("\n")
         )
         .setColor("#2B0B3F");
